eae8c8dadacc60111ec3af65ead5b109ca8e24c2
Marcin Bonka <marcin.bonka@gmail.com>
2020-06-14 16:02:12 +0200

    Text area updated automatically after site refresh, added arrow function to load saved text from local storage

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,39 @@
 import '../scss/main.scss';
 
-/* place your code below */
+/* Function prototypes */
+const LocalsLoad = () => {
+    const localText = localStorage.getItem('MyInputText');
+    if(localText)
+    {
+        console.log(`Local text under the key 'MyInputText' exists and will be loaded as textbox content `);
+        document.getElementById("myText").value = localText;
+        console.log(`Local text '${localText}' loaded.`);
+    }
+    else
+        console.log(`Local text under the key 'MyInputText' doesn't exist. Will be not loaded. `);
+}
 
+
+
+/* initial tasks */
 console.log('HELLO 🚀');
+LocalsLoad();
 
-const txtMyInput = document.querySelector('.form__text--js');
-
-
+/* query selectors */
 const buttonLoad = document.querySelector('.form__btnLoad--js');
+const buttonSave = document.querySelector('.form__btnSave--js');
+
+/* event listeners */
 buttonLoad.addEventListener('click', (e) => {
   console.log(`button ${buttonLoad.innerHTML} clicked `);
-  const localText = localStorage.getItem('MyInputText');
-  if(localText)
-  {
-    console.log(`Local text under the key 'MyInputText' exists and will be loaded as textbox content `);
-    document.getElementById("myText").value = localText;
-    console.log(`Local text '${localText}' loaded.`);
-  }
-  else
-    console.log(`Local text under the key 'MyInputText' doesn't exist. Will be not loaded. `);
+  LocalsLoad();
 });
 
-const buttonSave = document.querySelector('.form__btnSave--js');
 buttonSave.addEventListener('click', (e) => {
     console.log(`Button ${buttonSave.innerHTML} clicked `);
     const userText = document.getElementById("myText").value;
     localStorage.setItem('MyInputText', userText);  
     console.log(`Text '${userText}' saved to local storage.`);
 });
+
+
